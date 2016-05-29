05709e2273b22ce0c8961633a3bdcbcb0ced4892
cinashp <cinashp@outlook.com>
2016-05-29 21:31:19 +0530

    Removed debug messages

--- yowsup/demos/meaningclient/tweet.py
+++ yowsup/demos/meaningclient/tweet.py
@@ -4,16 +4,12 @@ from application_only_auth import Client
 
 
 def gettweetsfromapi( messageBody ):
-    print ("I was here...")
     CONSUMER_KEY = getconsumerkey()
     CONSUMER_SECRET = getconsumersecret()
-    print (CONSUMER_KEY)
-    print (CONSUMER_SECRET)
 
     client = Client(CONSUMER_KEY, CONSUMER_SECRET)
 
     tweet = client.request('https://api.twitter.com/1.1/search/tweets.json?q=%23IPLfinal&count=5')
-    print json.dumps(tweet, sort_keys=True, indent=4, separators=(',', ':'))
 
     status = client.rate_limit_status()
     print status['resources']['search']
@@ -22,13 +18,10 @@ def gettweetsfromapi( messageBody ):
     
     parsed_json = tweet
     formattedstr = ""
-    print ("Json loaded")
     i = 1
     for status in parsed_json['statuses']:
         name = status['user']['screen_name']
-        print (name)
         message = status['text']
-        print (message)
         formattedstr = formattedstr + str(i) + ". *" + name + "*: "
         formattedstr = formattedstr + message + "\n\n"
         i=i+1
@@ -37,16 +30,12 @@ def gettweetsfromapi( messageBody ):
     return "Unknown error occured..."
 
 def getscorefromapi( messageBody ):
-    print ("I was here...")
     CONSUMER_KEY = getconsumerkey()
     CONSUMER_SECRET = getconsumersecret()
-    print (CONSUMER_KEY)
-    print (CONSUMER_SECRET)
 
     client = Client(CONSUMER_KEY, CONSUMER_SECRET)
 
     tweet = client.request('https://api.twitter.com/1.1/search/tweets.json?q=from:IPL&count=5')
-    print json.dumps(tweet, sort_keys=True, indent=4, separators=(',', ':'))
 
     status = client.rate_limit_status()
     print status['resources']['search']
@@ -55,13 +44,10 @@ def getscorefromapi( messageBody ):
     
     parsed_json = tweet
     formattedstr = ""
-    print ("Json loaded")
     i = 1
     for status in parsed_json['statuses']:
         name = status['user']['screen_name']
-        print (name)
         message = status['text']
-        print (message)
         formattedstr = formattedstr + str(i) + ". *" + name + "*: "
         formattedstr = formattedstr + message + "\n\n"
         i=i+1
