9c5b3a080154d71a06266e71b8cad46e06ce610f
cinashp <cinashp@outlook.com>
2016-05-31 06:34:02 +0530

    Any hashtag

--- yowsup/demos/meaningclient/tweet.py
+++ yowsup/demos/meaningclient/tweet.py
@@ -7,54 +7,40 @@ from application_only_auth import Client
 def gettweetsfromapi( messageBody ):
     CONSUMER_KEY = getconsumerkey()
     CONSUMER_SECRET = getconsumersecret()
-
     client = Client(CONSUMER_KEY, CONSUMER_SECRET)
 
-    tweet = client.request('https://api.twitter.com/1.1/search/tweets.json?q=%23IPLfinal&count=5')
+    hashtag = gethashtag(messageBody);
+    tweet = client.request('https://api.twitter.com/1.1/search/tweets.json?q=' + hashtag + '&count=5')
 
     status = client.rate_limit_status()
     print status['resources']['search']
 
-    #try:
+    return formattweet(tweet)
     
-    parsed_json = tweet
-    formattedstr = ""
-    i = 1
-    for status in parsed_json['statuses']:
-        name = status['user']['screen_name']
-        message = status['text']
-        formattedstr = formattedstr + str(i) + ". *" + name + "*: "
-        formattedstr = formattedstr + message + "\n\n"
-        i=i+1
-    return formattedstr
-    #except:
-    return "Unknown error occured..."
-
-def getscorefromapi( messageBody ):
-    CONSUMER_KEY = getconsumerkey()
-    CONSUMER_SECRET = getconsumersecret()
-
-    client = Client(CONSUMER_KEY, CONSUMER_SECRET)
-
-    tweet = client.request('https://api.twitter.com/1.1/search/tweets.json?q=from:IPL&count=5')
-
-    status = client.rate_limit_status()
-    print status['resources']['search']
-
-    #try:
+def formattweet(tweet):
+    try:
+        parsed_json = tweet
+        formattedstr = ""
+        i = 1
+        for status in parsed_json['statuses']:
+            name = status['user']['screen_name']
+            message = status['text']
+            formattedstr = formattedstr + str(i) + ". *" + name + "*: "
+            formattedstr = formattedstr + message + "\n\n"
+            i=i+1
+        return formattedstr
+    except Exception, e:
+        print e
+        return "Unknown error occured while fetching the tweet..."
     
-    parsed_json = tweet
-    formattedstr = ""
-    i = 1
-    for status in parsed_json['statuses']:
-        name = status['user']['screen_name']
-        message = status['text']
-        formattedstr = formattedstr + str(i) + ". *" + name + "*: "
-        formattedstr = formattedstr + message + "\n\n"
-        i=i+1
-    return formattedstr
-    #except:
-    return "Unknown error occured..."
+
+def gethashtag( messageBody ):
+    hashtags = set([word[1:] for word in messageBody.split(' ') if word.startswith("#")])
+    urlencodedhashtags = ""
+    for hashtag in set(hashtags):
+        urlencodedhashtags = urlencodedhashtags + "%23" + hashtag + "+"
+    print (urlencodedhashtags[:-1])
+    return urlencodedhashtags[:-1]
 
 def getconsumerkey():
     configParser = ConfigParser.RawConfigParser()   
