0a2db4cfc62c62e07f82a9a0318ffae8f6e8efb5
cinashp <cinashp@outlook.com>
2016-06-13 19:32:07 +0530

    Removing contacts_sync

--- yowsup/demos/meaningclient/layer.py
+++ yowsup/demos/meaningclient/layer.py
@@ -11,7 +11,6 @@ class MeaningLayer(YowInterfaceLayer):
     @ProtocolEntityCallback("message")
     def onMessage(self, messageProtocolEntity):
         try:
-            print ('I am here..')
             if messageProtocolEntity.getType() == 'text':
                 self.onTextMessage(messageProtocolEntity)
             elif messageProtocolEntity.getType() == 'media':
